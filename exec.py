974aa63476ba46530379de14246d04f301b6b723
TiagoBHeck <heckheck2@gmail.com>
2023-11-17 11:43:47 -0300

    final commit - No Exec docs

--- exec.py
+++ exec.py
@@ -1,11 +1,28 @@
 import asyncio
 
+import pandas as pd
 from classes.DfToDatabase import DfToDatabase
 from classes.SgsIndexes import Indexes
 
-if __name__ == '__main__': 
-  df = asyncio.run(Indexes.main())
-  engine = DfToDatabase(df = df)
 
-  result = engine.create_engine(engine.df)
-  print(result)
+class Exec():  
+  
+    def __init__(self) -> pd.DataFrame:
+        self.df = asyncio.run(Indexes.main())
+      
+      
+    def save_to_db(self, df:pd.DataFrame) -> int | None:
+        engine = DfToDatabase(self.df)
+        result = engine.create_engine(engine.df)
+        return result
+  
+
+if __name__ == '__main__': 
+    exec = Exec()
+    try:
+      rows = exec.save_to_db(exec.df)
+      print(f'Total number os rows inserted in database: {rows}')
+    except ValueError:
+      raise "ValueError raised on traying to save rows"
+    
+  
